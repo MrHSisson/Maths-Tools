9644231a0b136e86ad1f62400b6094a16ac8c15a
MrHSisson <harrysisson7@gmail.com>
2025-12-26 21:55:03 +0000

    Add route for SimplifyingRatiosTool

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import IntegersTool from './tools/IntegersTool'
 import RatioSharingTool from './tools/RatioSharingTool'
+import SimplifyingRatiosTool from './tools/SimplifyingRatiosTool'
 // import DoubleBracketsTool from './tools/DoubleBracketsTool'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/integers" element={<IntegersTool />} />
       <Route path="/ratio-sharing" element={<RatioSharingTool />} />
+      <Route path="/simplifying-ratios" element={<SimplifyingRatiosTool />} />
       {/* <Route path="/double-brackets" element={<DoubleBracketsTool />} /> */}
     </Routes>
   )
