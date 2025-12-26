975fe4310ea96b3e781cd8bed3a5eebae5f5d8b4
MrHSisson <harrysisson7@gmail.com>
2025-12-26 19:45:37 +0000

    Replace RatioTool with RatioSharingTool in routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import IntegersTool from './tools/IntegersTool'
-// import RatioTool from './tools/RatioTool'
+import RatioSharingTool from './tools/RatioSharingTool'
 // import DoubleBracketsTool from './tools/DoubleBracketsTool'
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/integers" element={<IntegersTool />} />
-      {/* <Route path="/ratio" element={<RatioTool />} /> */}
+      <Route path="/ratio-sharing" element={<RatioSharingTool />} />
       {/* <Route path="/double-brackets" element={<DoubleBracketsTool />} /> */}
     </Routes>
   )
