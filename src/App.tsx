86f877d51ae459d49fd5b7392450af3d2132a527
MrHSisson <harrysisson7@gmail.com>
2026-05-04 21:19:58 +0100

    Refactor algebra tools imports and routes
    Reorganized imports and routes for algebra tools in App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,30 @@ import LandingPage from './components/LandingPage'
 import IntegersTool from './tools/IntegersTool'
 import RatioSharingTool from './tools/RatioSharingTool'
 import SimplifyingRatiosTool from './tools/SimplifyingRatiosTool'
-import CompletingTheSquare from './tools/CompletingTheSquare'
 import EstimationTool from './tools/EstimationTool'
-import ExpandingDoubleBracketsFOIL from './tools/ExpandingDoubleBracketsFOIL'
-import ExpandingDoubleBracketsGRID from './tools/ExpandingDoubleBracketsGRID'
-import ExpandingSingleBracketsFOIL from './tools/ExpandingSingleBracketsFOIL'
-import ExpandingSingleBracketsGRID from './tools/ExpandingSingleBracketsGRID'
-import IterationTool from './tools/IterationTool'
 import RecipesTool from './tools/RecipesTool'
 import FractionToRatio from './tools/FractionToRatio';
 import PowersOfTen from './tools/PowersOfTen';
 import FractionsOfAmounts from './tools/FractionsOfAmounts';
-import SimultaneousEquations from './tools/SimultaneousEquations';
-import NonLinearSimEq from './tools/NonLinearSimEq';
-import SolvingLinearEquations from './tools/SolvingLinearEquations';
 
 //Generators
 
 import TimesTablesGenerator from './tools/Generators/TimesTablesGenerator';
 import NegativeOperationsGenerator from './tools/Generators/NegativeOperationsGenerator';
 import MultiplicationGenerator from './tools/Generators/MultiplicationGenerator';
+
+//Algebra
+
+import CompletingTheSquare from './tools/Algebra/CompletingTheSquare'
+import ExpandingDoubleBracketsFOIL from './tools/Algebra/ExpandingDoubleBracketsFOIL'
+import ExpandingDoubleBracketsGRID from './tools/Algebra/ExpandingDoubleBracketsGRID'
+import ExpandingSingleBracketsFOIL from './tools/Algebra/ExpandingSingleBracketsFOIL'
+import ExpandingSingleBracketsGRID from './tools/Algebra/ExpandingSingleBracketsGRID'
+import IterationTool from './tools/Algebra/IterationTool'
+import SimultaneousEquations from './tools/Algebra/SimultaneousEquations';
+import NonLinearSimEq from './tools/Algebra/NonLinearSimEq';
+import SolvingLinearEquations from './tools/Algebra/SolvingLinearEquations';
+
 
 //Geometry
 
@@ -45,27 +49,31 @@ function App() {
       <Route path="/integers" element={<IntegersTool />} />
       <Route path="/ratio-sharing" element={<RatioSharingTool />} />
       <Route path="/simplifying-ratios" element={<SimplifyingRatiosTool />} />
-      <Route path="/expanding-double-brackets-grid" element={<ExpandingDoubleBracketsGRID />} />
-      <Route path="/completing-the-square" element={<CompletingTheSquare />} />
       <Route path="/estimation" element={<EstimationTool />} />
-      <Route path="/expanding-double-brackets-foil" element={<ExpandingDoubleBracketsFOIL />} />
-      <Route path="/expanding-single-brackets-foil" element={<ExpandingSingleBracketsFOIL />} />
-      <Route path="/expanding-single-brackets-grid" element={<ExpandingSingleBracketsGRID />} />
-      <Route path="/iteration" element={<IterationTool />} />
       <Route path="/recipes" element={<RecipesTool />} />
       <Route path="/fraction-to-ratio" element={<FractionToRatio />} />
       <Route path="/powers-of-ten" element={<PowersOfTen />} />
       <Route path="/fractions-of-amounts" element={<FractionsOfAmounts />} />
-      <Route path="/simultaneous-equations-elimination" element={<SimultaneousEquations />} />
-      <Route path="/simultaneous-equations-substitution" element={<NonLinearSimEq />} />
-      <Route path="/solving-linear-equations" element={<SolvingLinearEquations />} />
-
+      
 
       //Generators
 
       <Route path="/timestables" element={<TimesTablesGenerator />} />
       <Route path="/negative-operations" element={<NegativeOperationsGenerator />} />
       <Route path="/multiplication-methods" element={<MultiplicationGenerator />} />
+
+      //Algebra 
+
+      <Route path="/expanding-double-brackets-grid" element={<ExpandingDoubleBracketsGRID />} />
+      <Route path="/completing-the-square" element={<CompletingTheSquare />} />
+      <Route path="/expanding-double-brackets-foil" element={<ExpandingDoubleBracketsFOIL />} />
+      <Route path="/expanding-single-brackets-foil" element={<ExpandingSingleBracketsFOIL />} />
+      <Route path="/expanding-single-brackets-grid" element={<ExpandingSingleBracketsGRID />} />
+      <Route path="/iteration" element={<IterationTool />} />
+      <Route path="/simultaneous-equations-elimination" element={<SimultaneousEquations />} />
+      <Route path="/simultaneous-equations-substitution" element={<NonLinearSimEq />} />
+      <Route path="/solving-linear-equations" element={<SolvingLinearEquations />} />
+
 
       //Geometry
       
